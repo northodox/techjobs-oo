188c5d8694e353859ef00f06975c5d0ea3479902
Chris <northodontist@gmail.com>
2019-06-16 20:31:43 -0500

    Initial commit, switched to Java 1.8 in project setup (so it will...work)

--- src/main/java/org/launchcode/TechjobsApplication.java
+++ src/main/java/org/launchcode/TechjobsApplication.java
@@ -2,7 +2,7 @@ package org.launchcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//blorp
 @SpringBootApplication
 public class TechjobsApplication {
 
