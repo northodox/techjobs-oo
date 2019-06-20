5f80d8585b8ed03e8fd3c786b821a70aa8a169b8
Chris <northodontist@gmail.com>
2019-06-20 12:56:45 -0500

    Added the code to populate the remaining ArrayList collections for the view

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -41,10 +41,11 @@ public class JobForm {
 
         JobData jobData = JobData.getInstance();
 
-            //TODO #4 - populate the other ArrayList collections needed in the view
+            //populate the other ArrayList collections needed in the view
         employers = jobData.getEmployers().findAll();
-
-
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
     }
 
     public String getName() {
